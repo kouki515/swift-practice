62587caa04f0a4b1c4c022ff3ea0f0795fcfe5a1
kouki515 <71614240+kouki515@users.noreply.github.com>
2022-07-01 05:24:38 +0900

    Initial Commit

---
+++ Shared/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  Shared
+//
+//  Created by kouki on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
